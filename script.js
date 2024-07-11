d42f052581db74c7220c40bfb186a9385d26ea11
AbdulHaseb9 <haseebsheikh2380@gmail.com>
2024-07-11 14:47:26 -0700

    changes in mobile screen

--- script.js
+++ script.js
@@ -3,13 +3,6 @@ var inp = document.querySelector("#input");
 
 for (const btns of button) {
     btns.addEventListener("click", (e) => {
-        // if (e.target.value === "=") {
-        //     document.querySelector(".history").innerHTML = inp.value
-        //     inp.value = eval(inp.value);
-        //     return;
-        // }
-        // console.log(e.target.value);
-        // inp.value += e.target.value;
         switch (e.target.value) {
             case "=":
                 document.querySelector(".history").innerHTML = inp.value
